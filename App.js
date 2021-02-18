49d0872b932fbdf0245b4fdb1332ce74f223d285
adebisiii <34288825+adebisiii@users.noreply.github.com>
2021-02-18 15:32:42 +0300

    finish

--- App.js
+++ App.js
@@ -1,28 +1,34 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
 import * as Location from "expo-location";
-import UnitsPicker from './components/UnitsPicker';
+import UnitsPicker from "./components/UnitsPicker";
 
-import WeatherInfo from './components/WeatherInfo';
+import WeatherInfo from "./components/WeatherInfo";
+import ReloadIcon from "./components/ReloadIcon";
+import WeatherDetails from "./components/WeatherDetails";
+import { colors } from "./utils/index";
+import { WEATHER_API_KEY } from "react-native-dotenv";
 
-const WEATHER_API_KEY = '6b97b7206070e57917dc9ca3cff5a522';
-const BASE_WEATHER_URL = 'https://api.openweathermap.org/data/2.5/weather?';
+const BASE_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather?";
 
 export default function App() {
   const [errorMessage, setErrorMessage] = useState(null);
   const [currentWeather, setCurrentWeather] = useState(null);
-  const [unitsSystem, setUnitsSystem] = useState('metric');
+  const [unitsSystem, setUnitsSystem] = useState("metric");
 
   useEffect(() => {
     load();
-  }, [unitsSystem])
+  }, [unitsSystem]);
 
   async function load() {
+    setCurrentWeather(null);
+    setErrorMessage(null);
+
     try {
       let { status } = await Location.requestPermissionsAsync();
 
-      if (status != 'granted') {
+      if (status != "granted") {
         console.log(status);
         setErrorMessage("Çalışması için lokasyona izin verin..");
         return;
@@ -31,53 +37,60 @@ export default function App() {
       const location = await Location.getCurrentPositionAsync();
       const { latitude, longitude } = location.coords;
 
-      console.log(latitude, longitude)
+      console.log(latitude, longitude);
 
-      const weatherUrl =
-        `${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=${unitsSystem}&appid=${WEATHER_API_KEY}`;
-
-
-
+      const weatherUrl = `${BASE_WEATHER_URL}lat=${latitude}&lon=${longitude}&units=${unitsSystem}&appid=${WEATHER_API_KEY}`;
 
       const response = await fetch(weatherUrl);
 
       const result = await response.json();
 
-
       if (response.ok) {
         setCurrentWeather(result);
-        console.log(result)
+        console.log(result);
       } else {
         setErrorMessage(result.message);
-        console.log('response içindeki hata ${result.message}')
+        console.log("response içindeki hata ${result.message}");
       }
 
       console.log(latitude, longitude);
     } catch (error) {
-      setErrorMessage(error.message)
+      setErrorMessage(error.message);
     }
   }
 
   if (currentWeather) {
- 
     return (
       <View style={styles.container}>
         <StatusBar style="auto" />
-        <View style={styles.main} >
-          <UnitsPicker  unitsSystem= {unitsSystem}  setUnitsSystem= {setUnitsSystem} />
+        <View style={styles.main}>
+          <UnitsPicker
+            unitsSystem={unitsSystem}
+            setUnitsSystem={setUnitsSystem}
+          />
+          <ReloadIcon load={load} />
           <WeatherInfo currentWeather={currentWeather} />
         </View>
-
-
+        <WeatherDetails currentWeather={currentWeather} />
+        <WeatherDetails
+          currentWeather={currentWeather}
+          unitsSystem={unitsSystem}
+        />
+      </View>
+    );
+  } else if (errorMessage) {
+    return (
+      <View style={styles.container}>
+        <ReloadIcon load={load} />
+        <Text style={styles.errorText}>{errorMessage} garip bir hata</Text>
+        <StatusBar style="auto" />
       </View>
     );
   } else {
     return (
       <View style={styles.container}>
-        <StatusBar style="auto" />
-        <Text style={styles.errorText} >{errorMessage} garip bir hata</Text>
-
-
+        <ActivityIndicator size="large" color={colors.PRIMARY_COLOR} />
+        <Text style={styles.errorText}>{errorMessage} garip bir hata</Text>
       </View>
     );
   }
@@ -93,6 +106,6 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   errorText: {
-    textAlign: "center"
-  }
+    textAlign: "center",
+  },
 });
--- components/UnitsPicker.js
+++ components/UnitsPicker.js
@@ -1,14 +1,36 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-import { Picker } from '@react-native-community/picker'
+import React from "react";
+import { View, StyleSheet, Platform } from "react-native";
+import { Picker } from "@react-native-community/picker";
 
-export default function UnitsPicker({unitsSystem, setUnitsSystem}) {
-    return (
-        <View>
-            <Picker selectedValue={unitsSystem} onValueChange = {(item) => setUnitsSystem(item)} >
-                <Picker.Item label = "C°" value = "metric" />
-                <Picker.Item label = "F°" value = "imperial" />
-            </Picker>
-        </View>
-    )
+export default function UnitsPicker({ unitsSystem, setUnitsSystem }) {
+  return (
+    <View style={styles.unitsSystem}>
+      <Picker
+        selectedValue={unitsSystem}
+        onValueChange={(item) => setUnitsSystem(item)}
+        mode="dropdown"
+        itemStyle={{ fontSize: 12 }}
+      >
+        <Picker.Item label="C°" value="metric" />
+        <Picker.Item label="F°" value="imperial" />
+      </Picker>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  unitsSystem: {
+    position: "absolute",
+    height: 50,
+    width: 100,
+    left: 20,
+    ...Platform.select({
+      ios: {
+        top: -30,
+      },
+      android: {
+        top: 30,
+      },
+    }),
+  },
+});
